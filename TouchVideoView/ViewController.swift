7e84a2d800472ae48d280ffe7c7c3ea328039715
Игорь Талов <igortalov@gmail.com>
2019-01-26 17:36:41 +0300

    Initial Commit

---
+++ TouchVideoView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TouchVideoView
+//
+//  Created by Игорь Талов on 26/01/2019.
+//  Copyright © 2019 IgorTalov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
